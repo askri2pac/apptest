431f5cfc6626f780343e73d144ab437bad737df8
root <nassimaskri@gmail.com>
2018-05-23 13:00:37 +0100

    home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import {ContactsModule} from './modules/contacts/contacts.module';
 
 const appRoutes: Routes = [
       {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
         path: 'home',
         loadChildren: 'app/modules/home/home.module#HomeModule'
       },
+       {
+        path: 'contact',
+        loadChildren: 'app/modules/contacts/contacts.module#ContactsModule'
+       }
 ];
 
 export const AppModuleRouting = RouterModule.forRoot(appRoutes);
