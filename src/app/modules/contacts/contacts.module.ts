284a7e6834c59e3289a38e691ce9e7183e3ea7e0
root <nassimaskri@gmail.com>
2018-05-28 16:05:06 +0100

    search-engine-front

--- src/app/modules/contacts/contacts.module.ts
+++ src/app/modules/contacts/contacts.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts/contacts.component';
-import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   imports: [
     CommonModule,
     ContactsRoutingModule,
-    MatInputModule,
   ],
   declarations: [ContactsComponent]
 })
