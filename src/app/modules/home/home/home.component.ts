042d5ebcb9cf48aae80570c9af482acd975097b6
root <nassimaskri@gmail.com>
2018-05-29 12:43:30 +0100

    historic search

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -12,18 +12,19 @@ export class HomeComponent implements OnInit {
     showCurrentLocation: false,
     resOnSearchButtonClickOnly: true,
     inputPlaceholderText: 'Où : Lille, Alsace, Bd Voltaire, ...',
-    recentStorageName: 'componentData3'
+    recentStorageName: 'componentData3',
+    showSearchButton: false,
   };
 
   constructor() {
-    setTimeout(() => {
+   /* setTimeout(() => {
       this.userSettings3['inputPlaceholderText'] = 'This is delayed test';
       this.userSettings3 = Object.assign({}, this.userSettings3);
     }, 5000);
     setTimeout(() => {
       this.userSettings3['inputString'] = 'Bangalore, karnataka';
       this.userSettings3 = Object.assign({}, this.userSettings3);
-    }, 10000);
+    }, 10000);*/
   }
 
   ngOnInit() {
